b5351f9fca99d69bf162c6b9250cbb40e35a684c
ElishJ <ElishJ@cobank.com>
2019-10-18 09:26:17 -0600

    Initial Commit of Java Server

---
+++ src/main/java/example/camel/spring/restapi/route/RESTServer.java
@@ -0,0 +1,30 @@
+package example.camel.spring.restapi.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+//Make sure to always declare as component
+@Component
+public class RESTServer extends RouteBuilder {
+    @Override
+    public void configure() {
+
+        //Configure the REST server. Utilizing spark-rest as server component due to ease of use
+        //with straightforward REST API
+        restConfiguration().component("spark-rest").port(8081);
+
+        rest().get("playerscore/{playerID}")
+                //Declares that this endpoint from here on is treated as a route vs REST endpoint
+                .route()
+                //Has to be of type Expression when setting body. Using simple expression
+                .setBody(simple("You requested information on ${header.playerID}"))
+                .log(LoggingLevel.INFO,"The user requested to know the score for user ${header.playerID}");
+
+        rest().post("/submitnewscore")
+                //Declares that this endpoint from here on is treated as a route vs REST endpoint
+                .route()
+                .log(LoggingLevel.INFO, "You sent me the payload of ${body}")
+                .setBody(simple("You sent me the payload of ${body}"));
+    }
+}
